b074db06efb4ab1ddab484515fe6345bf7936337
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-01-20 17:11:16 +0100

    Fix javadoc
    Fix javadoc

--- axon-reactor/src/main/java/org/axonframework/extensions/reactor/commandhandling/gateway/ReactorCommandGateway.java
+++ axon-reactor/src/main/java/org/axonframework/extensions/reactor/commandhandling/gateway/ReactorCommandGateway.java
@@ -26,10 +26,10 @@ public interface ReactorCommandGateway extends ReactorMessageDispatchInterceptor
 
     /**
      * Sends the given {@code command} once the caller subscribes to the command result. Returns immediately.
-     * <p/>
+     * <p>
      * The given {@code command} is wrapped as the payload of a {@link CommandMessage} that is eventually posted on the
-     * {@link CommandBus}, unless the {@code command} already implements {@link Message}. In that case, a
-     * {@code CommandMessage} is constructed from that message's payload and {@link MetaData}.
+     * {@link CommandBus}, unless the {@code command} already implements {@link Message}. In that case, a {@code
+     * CommandMessage} is constructed from that message's payload and {@link MetaData}.
      *
      * @param command the command to dispatch
      * @param <R>     the type of the command result
@@ -44,7 +44,6 @@ public interface ReactorCommandGateway extends ReactorMessageDispatchInterceptor
      * @param commands a Publisher stream of commands to be dispatched
      * @return a Flux of command results. An ordering of command results corresponds to an ordering of commands being
      * dispatched
-     *
      * @see #send(Object)
      * @see Flux#concatMap(Function)
      */
